3edca273c0d96313cbf9c491b62a265d54a1e931
cbl980226 <binglinchen98@gmail.com>
2023-03-26 22:58:03 +0800

    feat(core): :sparkles: create hooks lib

---
+++ libs/hooks/src/lib/hooks.tsx
@@ -0,0 +1,12 @@
+/* eslint-disable-next-line */
+export interface HooksProps {}
+
+export function Hooks(props: HooksProps) {
+  return (
+    <div>
+      <h1>Welcome to Hooks!</h1>
+    </div>
+  )
+}
+
+export default Hooks
