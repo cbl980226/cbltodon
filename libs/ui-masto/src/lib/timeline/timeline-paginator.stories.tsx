bd8fdead53deefa73afb9b4b81d08b5306cd1f94
cbl980226 <binglinchen98@gmail.com>
2023-03-28 23:34:27 +0800

    feat(ui-masto): :sparkles: add TimelinePaginator component for handling pagination in timeline

---
+++ libs/ui-masto/src/lib/timeline/timeline-paginator.stories.tsx
@@ -0,0 +1,21 @@
+import { Meta, StoryObj } from '@storybook/react'
+import TimelinePaginator from './timeline-paginator'
+import { login } from 'masto'
+
+const masto = await login({
+  url: 'https://mastodon.social/'
+})
+
+const paginator = masto.v1.timelines.listPublic({ limit: 10 })
+
+const meta: Meta = {
+  title: 'Timeline/Paginator'
+}
+
+export default meta
+
+type Story = StoryObj<typeof TimelinePaginator>
+
+export const Demo: Story = {
+  render: () => <TimelinePaginator paginator={paginator} />
+}
---
+++ libs/ui-masto/src/lib/timeline/timeline-paginator.tsx
@@ -0,0 +1,20 @@
+import { mastodon, Paginator } from 'masto'
+import { FC } from 'react'
+import CommonPaginator from '../common/common-paginator'
+
+interface TimelinePaginatorProps {
+  paginator: Paginator<
+    mastodon.v1.Status[],
+    mastodon.v1.ListAccountStatusesParams
+  >
+}
+
+const TimelinePaginator: FC<TimelinePaginatorProps> = ({ paginator }) => {
+  return (
+    <CommonPaginator paginator={paginator}>
+      {status => <div>{JSON.stringify(status)}</div>}
+    </CommonPaginator>
+  )
+}
+
+export default TimelinePaginator
